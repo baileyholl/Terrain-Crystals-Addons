fd31913fdab652ad68c49d2edff464af9702e437
Bailey Hollingsworth <bailey.a.hollingsworth-1@ou.edu>
2016-04-06 23:19:17 -0600

    Origin, Grove, Cherry

--- java/com/drasticdemise/TCaddons/common/InitItems.java
+++ java/com/drasticdemise/TCaddons/common/InitItems.java
@@ -1,6 +1,7 @@
 package com.drasticdemise.TCaddons.common;
 
 import com.drasticdemise.TCaddons.common.items.ItemCherryBlossom;
+import com.drasticdemise.TCaddons.common.items.ItemMysticGrove;
 import com.drasticdemise.TCaddons.common.items.ItemOriginIsland;
 
 import net.minecraftforge.fml.relauncher.Side;
@@ -9,9 +10,13 @@ import net.minecraftforge.fml.relauncher.SideOnly;
 public class InitItems {
 	public static ItemCherryBlossom itemCherryBlossom;
 	public static ItemOriginIsland itemOriginIsland;
+	public static ItemMysticGrove itemMysticGrove;
+	
+	
 	public static void init(){
 		itemCherryBlossom = new ItemCherryBlossom();
 		itemOriginIsland = new ItemOriginIsland();
+		itemMysticGrove = new ItemMysticGrove();
 	}
 	public static void recipes(){
 
@@ -20,6 +25,7 @@ public class InitItems {
     public static void initModels() {
 		itemCherryBlossom.initModel();
 		itemOriginIsland.initModel();
+		itemMysticGrove.initModel();
 	}
 }
 	
