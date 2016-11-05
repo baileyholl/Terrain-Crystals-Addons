3f3bd8c8d1103271f06c411d036f9780b425a525
Bailey Hollingsworth <bailey.a.hollingsworth-1@ou.edu>
2016-11-04 21:49:45 -0600

    1.1.0 build

--- src/main/java/com/BaileyHollingsworth/TCaddons/common/TCAddons.java
+++ src/main/java/com/BaileyHollingsworth/TCaddons/common/TCAddons.java
@@ -13,7 +13,7 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 public class TCAddons {
     public static final String MODID = "terrainCrystalsAddons";
     public static final String MODNAME = "Terrain CrystalsAddons";
-    public static final String VERSION = "1.0.2";
+    public static final String VERSION = "1.1.0";
     public static final String URL = "https://raw.githubusercontent.com/DrasticDemise/Terrain-Crystals-Addons/master/1.9%20Update%20Handler";
     public static final String DEPENDENCIES = "required-after:terrainCrystals;required-after:BiomesOPlenty";
 
